0e7d68371134816946ffa6b7cafdf390bde24566
Johan Lindqvist <johan.lindqvist@forumvirium.fi>
2019-12-31 17:04:28 +0200

    Refs #3 - ApartmentSensor string representation update

--- backend/core/models.py
+++ backend/core/models.py
@@ -46,7 +46,7 @@ class ApartmentSensor(models.Model):
     identifier = models.CharField(max_length=255, unique=True)
 
     def __str__(self):
-        return f'Sensor in apartment {self.apartment}'
+        return f'Sensor {self.identifier} in apartment {self.apartment_id}'
 
 
 class ApartmentSensorValue(models.Model):
