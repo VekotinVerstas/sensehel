a747e17a9fc9c1072ed4592d0460d3f74b605def
Johan Lindqvist <johan.lindqvist@forumvirium.fi>
2020-01-07 16:13:33 +0200

    Code cleanup: removed unneeded / broken api endpoints & related scripts

--- backend/core/api/urls.py
+++ backend/core/api/urls.py
@@ -9,20 +9,15 @@ router.register(r'sensors', views.SensorViewSet)
 router.register(r'sensorattributes', views.SensorAttributeViewSet)
 router.register(r'services', views.ServiceViewSet)
 router.register(r'apartments', views.ApartmentViewSet, base_name='apartment-list')
-router.register(r'users', views.UserViewSet, base_name='user-list')
-router.register(
-    r'subscriptions', views.SubscriptionViewSet, base_name='subscriptions-list'
-)
+router.register(r'subscriptions', views.SubscriptionViewSet)
 router.register(r'apartmentsensors', views.ApartmentSensorViewSet)
-router.register(r'apartmentsensorvalues', views.ApartmentSensorValueViewSet)
 
 schema_view = get_schema_view(title='SenseHel API')
 
 # APIView does not and should not mix with routers
 urls = [
     url(r'schema/', schema_view),
-    url(r'available-services', views.ApartmentServiceList.as_view(), name='available-services'),
-    url(r'update-sensor-by-identifier', views.update_sensor_by_identifier),
+    url(r'available-services', views.AvailableServicesList.as_view(), name='available-services'),
     url(r'digita-gw', views.digita_gw, name='digita-gw'),
 ]
 
