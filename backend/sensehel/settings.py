9f022a8c4486649ce3d8e312f66c7de8bbe56aa8
Aapo (docker.fvh.io) Rista <aapris@gmail.com>
2022-03-28 22:51:21 +0300

    Remove SECRET_KEY

--- backend/sensehel/settings.py
+++ backend/sensehel/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'nt_d*)c*ixi!^gao-sw8rf3msw&v305%(g_!r1!w@a2=ms$jin'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
